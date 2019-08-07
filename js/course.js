772ab2d0426f6831370c256587a15e482abdf731
Verta-master <vertyagina@gmail.com>
2019-08-07 17:45:11 +0600

    Update 2019-08-07T11:45:03.404Z

---
+++ js/course.js
@@ -0,0 +1,1 @@
+$(".modal-table__line").click(function(){$(this).next().next().slideToggle(),$(this).next().toggleClass("modal-table__btn--open"),$(this).next().next().toggleClass("modal-table__list--open")});
--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+//Modal window list drop buttons
+$('.modal-table__line').click(function() {
+  $(this).next().next().slideToggle();
+  $(this).next().toggleClass('modal-table__btn--open');
+  $(this).next().next().toggleClass('modal-table__list--open');
+});
+
 //Dynamic favicon
 var isFirefox = typeof InstallTrigger !== 'undefined';
 
